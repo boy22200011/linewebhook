5bab207a268c09d193dec9695da0979b189c3e4a
nickhung <nick@microsys.com.tw>
2024-12-03 14:08:21 +0800

    增加API

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ import express from 'express'
 const app = express();
 app.use(express.json());
 
+
+app.get('/', (req,res)=>{
+    return res.status(200).json({code:0,message:"this is get!"})
+})
 app.post('/getTestStr', (req,res)=>{
   return res.status(200).json({code:0,message:"Hello World!"})
 })
