4093dc46095da158d1c9277c5ad523cda656d696
nickhung <nick@microsys.com.tw>
2024-12-03 14:30:47 +0800

    azure要求

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ app.post('/getTestStr', (req, res) => {
 });
 
 // 修改端口設定
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
