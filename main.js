910ce078a75ed44bcfc0e0581e7d41116acdb024
nickhung <nick@microsys.com.tw>
2024-12-03 13:56:11 +0800

    pnpm init

---
+++ main.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express();
+app.use(express.json());
+
+app.post('/getTestStr', (req,res)=>{
+  return res.status(200).json({code:0,message:"Hello World!"})
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
