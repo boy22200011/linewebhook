f2b96372980b5d8493bc242fae53a0373f43094a
nickhung <nick@microsys.com.tw>
2024-12-03 14:26:38 +0800

    azure要求

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
-import express from 'express'
+import express from 'express';
 
 const app = express();
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  return res.status(200).json({ code: 0, message: "this is get!" });
+});
 
-app.get('/', (req,res)=>{
-    return res.status(200).json({code:0,message:"this is get!"})
-})
-app.post('/getTestStr', (req,res)=>{
-  return res.status(200).json({code:0,message:"Hello World!"})
-})
+app.post('/getTestStr', (req, res) => {
+  return res.status(200).json({ code: 0, message: "Hello World!" });
+});
 
-const PORT = 3000;
+// 修改端口設定
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
