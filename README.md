daef88d2031168fbafe1862b9921880ab943aa01
nickhung <nick@microsys.com.tw>
2024-12-10 14:59:34 +0800

    feat: try postback

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# linewebhook
-test line webhook on microsoft azure
+# How to Start
+
+## 前置作業
+
+必須先下載安裝node 並執行
+
+``` shell
+npm install pnpm
+```
+
+## 安裝&啟動
+
+### 用pnpm安裝需要的package
+
+``` shell
+sudo pnpm install
+```
+
+## 啟動
+
+``` shell
+pnpm start
+```
--- src/modules/line/line.service.ts
+++ src/modules/line/line.service.ts
@@ -21,11 +21,41 @@ export class LineService {
 
   async handleEvent(events: WebhookEvent[]): Promise<any> {
     for (let event of events) {
+      if (event.type === "postback") {
+        await this.client.replyMessage(event.replyToken, {
+          type: "template",
+          altText: `這是範例的 Buttons 模板訊息`,
+          template: {
+            type: "buttons",
+            title: "前往官網",
+            text: "請點選",
+            actions: [
+              {
+                type: "uri",
+                label: "前往網站",
+                uri: "https://www.microsys.com.tw/"
+              }
+            ]
+          }
+        })
+      }
       if (event.type === "message") {
         if (event.message.type === "text") {
           await this.client.replyMessage(event.replyToken, {
-            type: "text",
-            text: `You said: ${event.message.text}`
+            type: "template",
+            altText: `這是範例的 Buttons 模板訊息`,
+            template: {
+              type: "buttons",
+              title: "您好有問題請洽詢",
+              text: "洽詢",
+              actions: [
+                {
+                  type: "postback",
+                  label: "前往官網",
+                  data: "action=option1"
+                }
+              ]
+            }
           })
         }
       }
