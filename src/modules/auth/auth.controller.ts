e6f742a7de3cd766cf3554a63006bcb56414cd02
nickhung <nick@microsys.com.tw>
2024-12-16 16:42:55 +0800

    feat: 技安機器人
    1. 綁定PPEID前置作業

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,13 +4,13 @@ import { AuthService } from "./auth.service"
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post("login")
-  async login(@Body() user: { username: string; password: string }) {
-    return this.authService.login(user)
-  }
+  // @Post("login")
+  // async login(@Body() user: { username: string; password: string }) {
+  //   return this.authService.login(user)
+  // }
 
-  @Post("getTokenData")
-  async getTokenData(@Body() obj: { token: string }) {
-    return await this.authService.getTokenData(obj.token)
-  }
+  // @Post("getTokenData")
+  // async getTokenData(@Body() obj: { token: string }) {
+  //   return await this.authService.getTokenData(obj.token)
+  // }
 }
--- src/modules/line/line.service.ts
+++ src/modules/line/line.service.ts
@@ -7,7 +7,7 @@ import {
   JoinEvent,
   LeaveEvent,
   TextEventMessage,
-  ReplyableEvent
+  EventSource
 } from "@line/bot-sdk"
 import { ConfigService } from "@nestjs/config"
 
@@ -25,17 +25,15 @@ export class LineService {
 
   // 事件處理器映射表，用來處理不同的事件類型
   private readonly eventHandlers: Record<EventType, (event: WebhookEvent) => Promise<void>> = {
-    [EventType.MESSAGE]: (event: MessageEvent) => this.messageHandler(event),
-    [EventType.POSTBACK]: (event: PostbackEvent) => this.postBackHandler(event),
-    [EventType.JOIN]: (event: JoinEvent) => this.joinGroupHandler(event),
-    [EventType.LEAVE]: (event: LeaveEvent) => this.leaveGroupHandler(event)
+    [EventType.MESSAGE]: (event: WebhookEvent & MessageEvent) => this.messageHandler(event),
+    [EventType.POSTBACK]: (event: WebhookEvent & PostbackEvent) => this.postBackHandler(event),
+    [EventType.JOIN]: (event: WebhookEvent & JoinEvent) => this.joinGroupHandler(event),
+    [EventType.LEAVE]: (event: WebhookEvent & LeaveEvent) => this.leaveGroupHandler(event)
   }
 
   // 指令處理器映射表，用來處理特定的文字指令
-  private readonly commandHandler: Record<string, () => any> = {
-    "@請假": () => this.leaveHandler(),
-    "@打卡": () => this.clockInHandler(),
-    "@綁定": () => this.bindHandler()
+  private readonly commandHandler: Record<string, (event: WebhookEvent & MessageEvent) => any> = {
+    "@綁定PPEID": (event) => this.bindHandler(event)
   }
 
   // 根據群組 ID 取得群組名稱
@@ -63,18 +61,20 @@ export class LineService {
   }
 
   // 回覆訊息
-  private async replyMessage(event: ReplyableEvent, options: any) {
+  private async replyMessage(event: WebhookEvent & MessageEvent, options: any) {
     await this.client.replyMessage(event.replyToken, options)
   }
 
   // 處理文字訊息事件
-  private async messageHandler(event: MessageEvent) {
+  private async messageHandler(event: WebhookEvent & MessageEvent) {
     let options
     try {
       switch (event.message.type) {
         case MessageType.TEXT:
-          options = await this.messageWithTextHandler(event.message)
+          options = await this.messageWithTextHandler(event, event.message)
           break
+        default:
+          return
       }
       await this.replyMessage(event, options)
     } catch (err) {
@@ -83,105 +83,78 @@ export class LineService {
   }
 
   // 處理 postback 事件
-  private async postBackHandler(event: PostbackEvent) {
+  private async postBackHandler(event: WebhookEvent & PostbackEvent) {
     console.log(event)
     // await this.replyMessage(event, this.createButtonTemplate())
   }
 
   // 進入群組事件的空處理器（未實作）
-  private async joinGroupHandler(event: JoinEvent) {
+  private async joinGroupHandler(event: WebhookEvent & JoinEvent) {
     console.log("Line bot join group")
   }
 
   // 離開群組事件的空處理器（未實作）
-  private async leaveGroupHandler(event: LeaveEvent) {
+  private async leaveGroupHandler(event: WebhookEvent & LeaveEvent) {
     console.log("Line bot leave group")
   }
 
   // 處理文字訊息並根據指令執行對應的處理
-  private async messageWithTextHandler(message: TextEventMessage) {
-    const handler = this.commandHandler[message.text]
+  private async messageWithTextHandler(event: WebhookEvent & MessageEvent, textEventMessage: TextEventMessage) {
+    const handler = this.commandHandler[textEventMessage.text]
     if (handler) {
-      return await handler()
-    }
-  }
+      // 有在指令陣列
+      return await handler(event)
+    } else {
+      const source: EventSource = event.source
 
-  // 處理「請假」指令並回傳按鈕模板訊息
-  private async leaveHandler() {
-    return {
-      type: "template",
-      altText: `永億資訊請假清單`,
-      template: {
-        type: "buttons",
-        title: "請假",
-        text: "-",
-        actions: [
-          {
-            type: "uri",
-            label: "請假",
-            uri: "https://www.microsys.com.tw/"
-          }
-        ]
+      if (source.type === "group") {
+        // 來自群組
+      } else if (source.type === "user") {
+        // 單純訊息
+        const message = textEventMessage.text
+
+        const regex = /^PPEID:\s*(\S+)$/
+
+        const match = message.match(regex)
+
+        if (match) {
+          const ppeid = match[1]
+
+          // TODO: ppeid 驗證是否合法
+
+          // TODO: DB操作
+
+          // 回應綁定成功
+          await this.replyMessage(event, { type: "text", text: `綁定成功` })
+        } else {
+          return this.replyInvalidFormat(event)
+        }
+      } else {
+        // 無法確定來源
+        // TODO: 這邊可以寫log
       }
     }
   }
 
-  // 處理「打卡」指令並回傳按鈕模板訊息
-  private async clockInHandler() {
-    return {
-      type: "template",
-      altText: `永億資訊遠距打卡`,
-      template: {
-        type: "buttons",
-        title: "打卡",
-        text: "-",
-        actions: [
-          {
-            type: "uri",
-            label: "打卡",
-            uri: "https://www.microsys.com.tw/"
-          }
-        ]
-      }
+  private async bindHandler(event: WebhookEvent & MessageEvent) {
+    const source: EventSource = event.source
+
+    // 先判斷是不是群組
+    if (source.type === "group") {
+    } else if (source.type === "user") {
+      // 來自個人
+      await this.client.pushMessage(event.source.userId, {
+        type: "text",
+        text: "請輸入PPEID，格式: PPEID: XXXXXX"
+      })
+    } else {
+      // 無法確定來源
+      // TODO: 這邊可以寫log
     }
   }
 
-  private async bindHandler() {
-    return {
-      type: "template",
-      altText: `綁定註冊`,
-      template: {
-        type: "buttons",
-        title: "與小威綁定",
-        text: "-",
-        actions: [
-          {
-            type: "postback",
-            label: "確定"
-          }
-        ]
-      }
-    }
-  }
-
-  // 建立通用的按鈕模板訊息
-  private createButtonTemplate() {
-    return {
-      type: "template",
-      altText: `這是範例的 Buttons 模板訊息`,
-      template: {
-        type: "buttons",
-        title: "前往官網",
-        text: "請點選",
-        actions: [
-          {
-            type: "uri",
-            label: "前往網站",
-            uri: "https://www.microsys.com.tw/"
-          }
-        ]
-      }
-    }
+  private async replyInvalidFormat(event: WebhookEvent & MessageEvent) {
+    return this.replyMessage(event, { type: "text", text: `請輸入正確的格式，例如 "PPEID: XXXXX"` })
   }
 }
 
